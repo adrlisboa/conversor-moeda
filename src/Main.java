16a2037d4215883a5eb213fa6b93a67143f18c62
Adriano <adriano.psv@hotmail.com>
2025-09-03 13:48:36 -0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,36 @@ public class Main {
             opcao = sc.nextInt();
 
             switch (opcao) {
+                case 1:
+                    System.out.println("Digite o valor em Real: R$ ");
+                    double real = sc.nextDouble();
+                    System.out.printf("Resultado: $ %.2f USD\n", convMoeda.realParaDolar(real));
+                    break;
 
+                case 2:
+                    System.out.println("Digite o valor em Dólar: $ ");
+                    double dolar = sc.nextDouble();
+                    System.out.printf("Resultado: R$ %.2f BRL\n", convMoeda.dolarParaReal(dolar));
+                    break;
+
+                case 3:
+                    System.out.println("Digite o valor em Real: R$ ");
+                    real = sc.nextDouble();
+                    System.out.printf("Resultado: £ %.2f EUR\n", convMoeda.realParaEuro(real));
+                    break;
+
+                case 4:
+                    System.out.println("Digite o valor em Euro: £ ");
+                    double euro = sc.nextDouble();
+                    System.out.printf("Resultado: R$ %.2f BRL\n", convMoeda.euroParaReal(euro));
+                    break;
+
+                case 0:
+                    System.out.println("Encerrando o programa...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
             }
         } while(opcao != 0);
 
